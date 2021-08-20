2eed84632238d0a6dcf77e33b23021d964747d0a
VartanovMaks <vartanov2009@gmail.com>
2021-08-20 12:03:34 +0300

    change upper menu through state

--- src/App.js
+++ src/App.js
@@ -4,22 +4,39 @@ import Game from './Components/Game'
 import Base from './Components/Base'
 import BaseDetails from './Components/BaseDetails'
 import Home from './Components/Home'
+import Header1 from './Components/Header1'
+import Header2 from './Components/Header2'
+import EmptyComponent from './Components/EmptyComponent'
 import {BrowserRouter as Router, Link, Redirect, Route, Switch} from 'react-router-dom'
+import React, {useState} from 'react';
+
+function Greeting(props) {
+  const [isBaseMenu, setIsBaseMenu]=useState(false);
+
+  if (!isBaseMenu) {    
+    return <Header1 lastClick={()=>setIsBaseMenu(!isBaseMenu)}/>;
+  }  
+  return <Header2 lastClick={()=>setIsBaseMenu(!isBaseMenu)}/>;
+}
+
 
 function App() {
+
+
   return (
     <div className="App">
       <Router>
-            <Link className="link-menu" to="/"  >Home</Link>
-            <Link className="link-menu" to="/rules" >Rules</Link>
-            <Link className="link-menu" to="/game" >Gam</Link>
-            <Link className="link-menu" to="/base" >Base</Link>
-        <Switch>
+            <Greeting />
+          <Switch>
             <Route path="/" exact component={Home}/> 
             <Route path="/rules" component={Rules}/> 
-            <Route path="/game" component={Game}/>
-            <Route path="/base" exact component={Base}/>
+            <Route path="/game" exact component={Game}/>
+            {/* <Route path="/base" exact component={Base}/> */}
+              <Route path="/base" exact>
+                <EmptyComponent lastClick={()=>console.log("hello")} />
+              </Route> 
             <Route path="/base/:id" component={BaseDetails}/>
+            <Redirect to='/rules' />
         </Switch>
       </Router>
      
