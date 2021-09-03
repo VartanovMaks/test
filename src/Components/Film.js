2bf8066da262e3003f4abc08557f2f77318ac2ff
VartanovMaks <vartanov2009@gmail.com>
2021-09-03 17:38:13 +0300

    Trailers work

--- src/Components/Film.js
+++ src/Components/Film.js
@@ -36,6 +36,12 @@ function Film(params) {
                             </div>
                     )}  
                 </div>
+                <iframe width="560" height="315" src={`${film.trailer}`}
+                        title="YouTube video player" frameborder="0" 
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+                        allowfullscreen>
+                </iframe>
+                
             </>}
         </div>
     );
